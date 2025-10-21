2d345c9adeacffdb19ed79049d75303196008b48
unlucky-shen <taufiqhaikal002@gmail.com>
2025-10-21 12:44:22 +0800

    Add tmux to applications list

--- configuration.nix
+++ configuration.nix
@@ -113,6 +113,7 @@ texlive.combined.scheme-full
 
 # ----- Apps -----
 kitty
+tmux
 spotify
 zathura
 gnome-tweaks
@@ -145,6 +146,6 @@ gnome-tweaks
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
+  system.stateVersion = "25.05";
 
 }
