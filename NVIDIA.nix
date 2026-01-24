d0b969935bd8e8fa1573199ddea5dee57864e817
Taufiq Haikal <taufiqhaikal002@gmail.com>
2026-01-24 10:08:58 +0000

    Backup nvidia config

---
+++ NVIDIA.nix
@@ -0,0 +1,44 @@
+{ config, pkgs, ... }: 
+{
+
+	boot.kernelParams = [ 
+		"nvidia-drm.modeset=1" 
+		"nvidia-drm.fbdev=1" 
+		"nvidia.NVreg_TemporaryFilePath=/var/tmp"
+	];
+
+	services.xserver.videoDrivers = [ 
+		"modesetting" 
+		"nvidia" 
+	];
+  
+	# fxck nvidia
+	hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    powerManagement.finegrained = true;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    
+		prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+
+	# fxck wayland too
+	environment.sessionVariables = {
+  	NIXOS_OZONE_WL = "1";
+	};
+
+	xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+  };
+
+}
